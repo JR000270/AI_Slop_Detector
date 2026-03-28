04b204a51bb345822ef2ba3f8b8a699b38bcf1a0
nyisaiah <143559947+nyisaiah@users.noreply.github.com>
2026-03-28 16:39:59 -0400

    add ai image detection functionality

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,11 @@ from fastapi import FastAPI
 import os
 import httpx
 from helper import get_ai_or_not_api_key
+import asyncio
 
 app = FastAPI()
+
+IMAGE =  "./images/Ballerina-Cappuccina.webp"
 API_KEY = get_ai_or_not_api_key()
 
 IMAGE_ENDPOINT = "https://api.aiornot.com/v2/image/sync"
@@ -20,3 +23,11 @@ async def send_image(image):
                 headers={"Authorization": f"Bearer {API_KEY}"},
                 files={"image": f}
             )
+            print("RESPONSE TYPE:", type(response))
+            print("RESPONSE JSON:", response.json())
+            return response.json()
+        
+if __name__ == "__main__":
+    result = asyncio.run(send_image(IMAGE))
+    
+    print(result)
