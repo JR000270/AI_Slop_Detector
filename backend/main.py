1c53a3337b9e5374979879fa1ccf1a439211af49
nyisaiah <143559947+nyisaiah@users.noreply.github.com>
2026-03-28 18:36:05 -0400

    updated main.py to remove unnecessary imports

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,5 @@
-from fastapi import FastAPI, UploadFile, File
-import os
-import httpx
-from helper import get_ai_or_not_api_key
-from gemini_functions import generate_text, describe_image, get_available_models
-import asyncio
-from pydantic import BaseModel
+from fastapi import FastAPI
+from routes import image
 
 app = FastAPI()
 app.include_router(image.router)
